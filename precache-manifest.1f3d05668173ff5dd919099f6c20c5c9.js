a4a739169238d6fbf08953e00d814c2f6ea82ae7
shuyin21 <51820731+shuyin21@users.noreply.github.com>
2020-10-27 06:48:14 +0000

    Updates

---
+++ precache-manifest.1f3d05668173ff5dd919099f6c20c5c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c040e7307c66d6863445afc2f89c267b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5de9c233b788932b29e8",
+    "url": "/monsters-rolodex/static/css/main.4c68c6a6.chunk.css"
+  },
+  {
+    "revision": "f446d9444977f330bfbe",
+    "url": "/monsters-rolodex/static/js/2.70969f69.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/monsters-rolodex/static/js/2.70969f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5de9c233b788932b29e8",
+    "url": "/monsters-rolodex/static/js/main.d352280c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
